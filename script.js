1f97a8055b942998d87d73bd410c606d27c62dd9
Surya-MERNstack <charusurya17@gmail.com>
2023-04-27 16:50:57 +0530

    errorsolved

--- script.js
+++ script.js
@@ -1,94 +1,92 @@
 
-let url = 'https://anapioficeandfire.com/api/books';
-
-
+const url = 'https://anapioficeandfire.com/api/books';
 
 async function fetchingBooks() {
-    try {
-      await fetch(url).then(((response) =>{
-       return  response.json()
-    })).then(
-        ((data) => {
-            let heading = document.createElement('h1');
-            heading.textContent = 'George Novels'
-            heading.setAttribute('id','heading')
-            data.map((items) => {
-            let displaybooks = document.getElementById('bookList');
-            displaybooks.innerHTML += " ";
-            
-            
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
 
-            //creating the div with class name book
-            let bookscontainer = document.createElement('div');
-            bookscontainer.classList.add('book')
-            
-            //book name
-            let name = document.createElement('h2');
-            name.textContent = ` ${items.name}`
-            
-            //Register Number
-            let register = document.createElement('p');
-            register.textContent = `ISBN : ${items.isbn}`
-            
-            //No of pages
-             let pages = document.createElement('p');
-             pages.textContent = `Number of Pages : ${items.numberOfPages}`
-             
-            //Character
-             let charactername = document.createElement('h3');
-             charactername.textContent = 'Characters'
-             let character = document.createElement('p');
-             character.textContent = ` ${items.characters.slice(0,5).join(', ')}...`
-             
-            //authors
-             let author = document.createElement('p');
-             author.textContent = `Authors : ${items.authors}`
+    const heading = document.createElement('h1');
+    heading.textContent = 'George Novels';
+    heading.setAttribute('id', 'heading');
 
-            //publisher
-             let publisher = document.createElement('p');
-             publisher.textContent = `Publisher : ${items.publisher}`
+    data.forEach(async (book) => {
+      const displayBooks = document.getElementById('bookList');
 
-            //reslased
-             let releasedate = document.createElement('p');
-             releasedate.textContent = `Released : ${items.released}`
-            
-             bookscontainer.appendChild(name);
-             bookscontainer.appendChild(register); 
-             bookscontainer.appendChild(pages); 
-             bookscontainer.appendChild(author); 
-             bookscontainer.appendChild(publisher);
-             bookscontainer.appendChild(releasedate);
-             bookscontainer.appendChild(charactername)
-             bookscontainer.appendChild(character); 
-             
-             displaybooks.appendChild(bookscontainer)
-             displaybooks.appendChild(heading)
-          
-             //filter the booksname and authorname
-            const searchInput = document.getElementById('search');
-            searchInput.addEventListener('keyup', event => {
-            const query = event.target.value.toLowerCase();
-           const books = displaybooks.getElementsByClassName('book');
-           Array.from(books).forEach(book => {
+      //creating the div with class name book
+      const booksContainer = document.createElement('div');
+      booksContainer.classList.add('book');
+
+      //book name
+      const name = document.createElement('h2');
+      name.textContent = book.name;
+
+      //ISBN
+      const register = document.createElement('p');
+      register.textContent = `ISBN: ${book.isbn}`;
+
+      //Number of pages
+      const pages = document.createElement('p');
+      pages.textContent = `Number of Pages: ${book.numberOfPages}`;
+
+      //Characters
+      const characterName = document.createElement('h3');
+      characterName.textContent = 'Characters';
+      const character = document.createElement('p');
+
+      for (const charUrl of book.characters.slice(0, 5)) {
+        const charRes = await fetch(charUrl);
+        const charData = await charRes.json();
+        character.textContent += `${charData.name}, `;
+      }
+
+      //Authors
+      const author = document.createElement('p');
+      author.textContent = `Authors: ${book.authors}`;
+
+      //Publisher
+      const publisher = document.createElement('p');
+      publisher.textContent = `Publisher: ${book.publisher}`;
+
+      //Release date
+      const releaseDate = document.createElement('p');
+      releaseDate.textContent = `Released: ${book.released}`;
+
+      booksContainer.appendChild(name);
+      booksContainer.appendChild(register);
+      booksContainer.appendChild(pages);
+      booksContainer.appendChild(author);
+      booksContainer.appendChild(publisher);
+      booksContainer.appendChild(releaseDate);
+      booksContainer.appendChild(characterName);
+      booksContainer.appendChild(character);
+
+      displayBooks.appendChild(booksContainer);
+      displayBooks.appendChild(heading);
+
+      //filter the books name and author name
+      const searchInput = document.getElementById('search');
+      searchInput.addEventListener('keyup', (event) => {
+        const query = event.target.value.toLowerCase();
+        const books = displayBooks.getElementsByClassName('book');
+        Array.from(books).forEach((book) => {
           const bookTitle = book.getElementsByTagName('h2')[0].textContent.toLowerCase();
-          const bookAuthors = book.getElementsByTagName('p')[4].textContent.toLowerCase();
+          const bookAuthors = book.getElementsByTagName('p')[5].textContent.toLowerCase();
           if (bookTitle.includes(query) || bookAuthors.includes(query)) {
             book.style.display = 'block';
           } else {
             book.style.display = 'none';
           }
         });
-        });
       });
-
-})
-        
-)
-}catch(err){
-    console.log(ReferenceError + err);
-}}
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 fetchingBooks();
+
 
 
 
